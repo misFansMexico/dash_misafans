ef5c889379ae75061feb3a9f30576ee47cc9b66e
Adrian Quiroz <mobvi68@gmail.com>
2020-09-25 13:08:59 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'dashmisFans';
+  @ViewChild('filePortada')
+  filePortada: ElementRef;
+  @ViewChild('filePerfil')
+  filePerfil: ElementRef;
+  @ViewChild('imgPortada')
+  imgPortada: ElementRef;
+  @ViewChild('imgPerfil')
+  imgPerfil: ElementRef;
+  @ViewChild('confirmSwal')
+  private confirmSwal: SwalComponent;
+  creador = {
+    nombre: '',
+    username: '',
+    description: '',
+    img_portada: '',
+    img_perfl: '',
+    account_twitch: '',
+    account_instagram: '',
+    account_twitter: '',
+    account_tiktok: '',
+  };
+  ProfileForm: FormGroup;
+
+  file: File | null = null;
+
+  constructor(public af_firestore: AngularFirestore) {}
+
+  onClickFileInputButton(numero: number): void {
+    if (numero == 1) this.filePortada.nativeElement.click();
+    else this.filePerfil.nativeElement.click();
+  }
+
+  onChangeFileInput(numero: number): void {
+    let files: { [key: string]: File };
+    let reader = new FileReader();
+
+    if (numero == 1) files = this.filePortada.nativeElement.files;
+    else files = this.filePerfil.nativeElement.files;
+    this.file = files[0];
+    reader.onload = (event: any) => {
+      if (numero == 1) {
+        this.imgPortada.nativeElement.src = event.target.result;
+        this.creador.img_portada = event.target.result;
+      } else {
+        this.imgPerfil.nativeElement.src = event.target.result;
+        this.creador.img_perfl = event.target.result;
+      }
+    };
+
+    reader.onerror = (event: any) => {
+      console.log('File could not be read: ' + event.target.error.code);
+    };
+
+    reader.readAsDataURL(this.file);
+  }
+
+  enviar() {
+    console.log(this.creador);
+    this.af_firestore
+      .collection('creadores')
+      .doc(`${this.creador.username}`)
+      .set(this.creador)
+      .then((data) => {
+        console.log(data);
+        this.confirmSwal.fire();
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    SweetAlert2Module.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
